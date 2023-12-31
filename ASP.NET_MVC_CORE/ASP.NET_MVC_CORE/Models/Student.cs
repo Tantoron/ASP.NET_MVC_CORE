4bf16579fd98335c51f42c183fdce2ba38abf22a
Tantoron <thanhtrong4501@gmail.com>
2023-12-31 18:22:20 +0700

    laptrinhweb02

---
+++ ASP.NET_MVC_CORE/ASP.NET_MVC_CORE/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_MVC_CORE.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public String Name{ get; set; }
+        public DateTime BirthDay { get; set; }
+        public int AddRess_Id { get; set; }
+        public String AddRess_Full { get; set; }
+    }
+    
+}
